060652cee172200ac6f96e12e9528be8029cf312
239_kab <239_kab@UR-K302-12>
2023-01-27 15:50:27 +0500

    Добавьте файлы проекта.

---
+++ DriverApp/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DriverApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Models.user12Entities context = new Models.user12Entities();
+
+        public MainWindow()
+        {
+            App.Current.Properties["context"] = context;
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource driversViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("driversViewSource")));
+            // Загрузите данные, установив свойство CollectionViewSource.Source:
+            driversViewSource.Source = context.Drivers.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new Windows.Drivers.Create().Show();
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+            System.Windows.Data.CollectionViewSource driversViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("driversViewSource")));
+
+            driversViewSource.Source = context.Drivers.ToList();
+        }
+
+        private void EditPhoto(object sender, RoutedEventArgs e)
+        {
+            var selectedDriver = driversDataGrid.SelectedItem as Models.Drivers;
+            OpenFileDialog open = new OpenFileDialog();
+            var result = open.ShowDialog();
+            if (result == true)
+            {
+                string filename = System.IO.Path.GetFileName(open.FileName);
+                string path1 = Path.GetFullPath("Images");
+                var path2 = path1.Substring(0, path1.Length - 17);
+                var path = path2 + @"\Images\" + filename;
+                System.IO.File.Copy(open.FileName, path, true);
+                selectedDriver.Photo = filename;
+                context.SaveChanges();
+
+                System.Windows.Data.CollectionViewSource driversViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("driversViewSource")));
+
+                driversViewSource.Source = context.Drivers.ToList();
+            }
+        }
+    }
+}
---
+++ DriverApp/Windows/Drivers/Create.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+using System.Linq;
+using System.IO;
+
+namespace DriverApp.Windows.Drivers
+{
+    /// <summary>
+    /// Логика взаимодействия для Create.xaml
+    /// </summary>
+    public partial class Create : Window
+    {
+        Models.user12Entities context = (Models.user12Entities)App.Current.Properties["context"];
+        Models.Drivers NewDriver = new Models.Drivers();
+
+        public Create()
+        {
+            InitializeComponent();
+
+            Driver.DataContext = NewDriver;
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            context.Drivers.Add(NewDriver);
+            
+
+
+            context.SaveChanges().ToString();
+            this.Close();
+          
+        }
+
+        private void surnameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void LoadImage(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            var result = open.ShowDialog();
+            if (result == true)
+            {
+                string filename = System.IO.Path.GetFileName(open.FileName);
+                string path1 = Path.GetFullPath("Images");
+                var path2 = path1.Substring(0, path1.Length - 17);
+                var path = path2 + @"\Images\" + filename;
+                 System.IO.File.Copy(open.FileName, path, true);
+                NewDriver.Photo = filename;
+               
+
+
+            }
+        }
+    }
+}
